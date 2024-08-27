e0b2a5e1d235468827bc5f5929b228fe5f6a7d2e
Rupesh Kumar Yadav <rupesh.official484@gmail.com>
2024-08-27 20:32:21 +0530

    commit

--- app/src/main/java/com/bussiness/pickup/riderStack/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bussiness/pickup/riderStack/ui/home/HomeFragment.kt
@@ -222,13 +222,6 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
                             android.Manifest.permission.ACCESS_COARSE_LOCATION
                         ) != PackageManager.PERMISSION_GRANTED
                     ) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
                         return
                     }
                     mMap.isMyLocationEnabled = true
@@ -273,6 +266,9 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
                 }
 
             }).check()
+
+        //Enable Zoom
+        mMap.uiSettings.isZoomControlsEnabled = true
 
         try {
             val success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(),R.raw.uber_maps_style))
